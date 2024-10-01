36364f0a3bacb894af424d287398f48800758f5d
random2907 <yarnjeetyadav1234567890@gmail.com>
2024-10-01 09:26:14 +0530

    init

---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <unistd.h>
+
+class Frame{
+        public:
+                void clear(){
+                        std::cout << "\e[2J\e[H";
+                }
+};
+
+int main(){
+        Frame f;
+        std::string s;
+        s = "     _    ____   ____ ___ ___      _         _        _             _     _           \n \
+   / \\  / ___| / ___|_ _|_ _|    / \\   _ __| |_     / \\   _ __ ___| |__ (_)_   _____ \n\
+   / _ \\ \\___ \\| |    | | | |    / _ \\ | '__| __|   / _ \\ | '__/ __| '_ \\| \\ \\ / / _ \\ \n\
+  / ___ \\ ___) | |___ | | | |   / ___ \\| |  | |_   / ___ \\| | | (__| | | | |\\ V /  __/ \n\
+ /_/   \\_\\____/ \\____|___|___| /_/   \\_\\_|   \\__| /_/   \\_\\_|  \\___|_| |_|_| \\_/ \\___|";
+        std::string bold="\e[5m";
+        std::string red_fg="\e[38;2;255;0;0m";
+        std::string green_fg="\e[38;2;0;255;0m";
+        std::string blue_fg="\e[38;2;0;0;255m";
+        std::string reset="\e[0m";
+        std::string color[]={red_fg,green_fg,blue_fg};
+        int x=0;
+        while (1){
+                std::cout << color[x];
+                std::cout << bold;
+                std::cout << s;
+                std::cout << reset << std::endl;
+                f.clear();
+                if (x!=2){
+                        x++;
+                }else{
+                        x=0;
+                }
+                sleep(1);
+        }
+
+        return 0;
+
+}
+                
+        
